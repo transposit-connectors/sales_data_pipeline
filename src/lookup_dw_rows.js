eb32f29f66a1cca78f521ab7ede8f7a66d741a40
Dan Moore <mooreds@transposit.com>
2019-10-24 21:42:37 +0000

    added dw row

---
+++ src/lookup_dw_rows.js
@@ -0,0 +1,14 @@
+(params) => {
+  const dw_rows = api.run("this.execute_dw_query")[0].rows;
+  const arr = [];
+  
+  // convert to row based format from API
+  dw_rows.forEach(r => {
+    const one_row = [];
+    r.f.forEach(v => {
+      one_row.push(v.v);
+    })
+    arr.push(one_row);
+  });
+  return arr;
+}
