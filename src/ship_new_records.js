7233cc53b0ee7fafbbaf9708559fb9f01ec63c28
Dan Moore <mooreds@transposit.com>
2019-10-29 19:09:14 +0000

    pass in numbers as numbers?

--- src/ship_new_records.js
+++ src/ship_new_records.js
@@ -14,14 +14,14 @@
     parameters.sales_channel = r[3];
     parameters.order_priority = r[4];
     parameters.order_date = r[5];
-    parameters.order_id = r[6];
+    parameters.order_id = parseInt(r[6]);
     parameters.ship_date = r[7];
-    parameters.units_sold = r[8];
-    parameters.unit_price = r[9];
-    parameters.unit_cost = r[10];
-    parameters.total_revenue = r[11];
-    parameters.total_cost = r[12];
-    parameters.total_profit = r[13];
+    parameters.units_sold = parseInt(r[8]);
+    parameters.unit_price = parseFloat(r[9]);
+    parameters.unit_cost = parseFloat(r[10]);
+    parameters.total_revenue = parseFloat(r[11]);
+    parameters.total_cost = parseFloat(r[12]);
+    parameters.total_profit = parseFloat(r[13]);
     
     one_operation.parameters = parameters;
     console.log(parameters);
