5bddf7f021c4b44889af7f10cfc7ec63d49a82ef
Dan Moore <mooreds@transposit.com>
2019-10-30 16:25:13 +0000

    works for one record now.

--- src/ship_new_records.js
+++ src/ship_new_records.js
@@ -1,4 +1,5 @@
 (params) => {
+  const moment = require('moment.js');
   const data = params.data;
   //console.log(data);
   const operations = [];
@@ -15,23 +16,30 @@
     parameters.item_type = r[2];
     parameters.sales_channel = r[3];
     parameters.order_priority = r[4];
-    parameters.order_date = '2010-10-10'; //r[5].replace(/\//g,"-");
+    parameters.order_date = moment(r[5]).format('Y-M-D');
     parameters.order_id = parseInt(r[6]);
-    parameters.ship_date = '2010-10-10'; //r[7].replace(/\//g,"-");
+    parameters.ship_date = moment(r[7]).format('Y-M-D');
     parameters.units_sold = parseInt(r[8]);
     parameters.unit_price = parseFloat(r[9]);
     parameters.unit_cost = parseFloat(r[10]);
     parameters.total_revenue = parseFloat(r[11]);
     parameters.total_cost = parseFloat(r[12]);
     parameters.total_profit = parseFloat(r[13]);
+    
     let sql = "SELECT * FROM gcp_bigquery.execute_query WHERE projectId='sales-data-pipeline' AND $body=(SELECT { 'useLegacySql' : false, 'query' : ";
-    const bqsql = "INSERT into `default.orderdata` (Region, Country, Item_Type, Sales_Channel, Order_Priority, Order_Date, Order_ID, Ship_Date, Units_Sold, Unit_Price, Unit_Cost, Total_Revenue, Total_Cost, Total_Profit) VALUES (@region,@country,@item_type, @sales_channel, @order_priority, @order_date, @order_id, @ship_date, @units_sold, @unit_price, @unit_cost, @total_revenue, @total_cost, @total_profit)";
-    sql = sql + "'" +bqsql + "'" + "  })";
+    // const bqsql = "INSERT into `default.orderdata` (Region) VALUES (@region)";
+    //const bqsql = "INSERT into `default.orderdata` (Region) VALUES ('"+parameters.region+"')";
+    const bqsql = "INSERT into `default.orderdata` (Region, Country, Item_Type, Sales_Channel, Order_Priority, Order_Date, Order_ID, Ship_Date, Units_Sold, Unit_Price, Unit_Cost, Total_Revenue, Total_Cost, Total_Profit) VALUES ('"
+        +parameters.region+"','"+parameters.country+"','"+parameters.item_type+"', '"+parameters.sales_channel+"', '"
+        +parameters.order_priority+"', '"+parameters.order_date+"', "+parameters.order_id+", '"+parameters.ship_date+"', "
+        +parameters.units_sold+", "+parameters.unit_price+", "+parameters.unit_cost+", "+parameters.total_revenue+", "
+        +parameters.total_cost+", "+parameters.total_profit+")";
+    sql = sql + '"' +bqsql + '"' + "  })";
     
     one_operation.parameters = parameters;
-    console.log(parameters);
+   // console.log(parameters);
     console.log(sql);
-    api.query(sql,parameters);
+    return api.query(sql,parameters);
     //operations.push(one_operation);
     try {
       //api.run("this.execute_dw_insert", parameters);
